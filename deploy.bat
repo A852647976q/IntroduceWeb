2cf0eec1486b65cec0e81e5da44e659e98cdaaa0
A852647976q <serens9528@gmail.com>
2026-03-31 01:54:17 +0800

    11222

--- deploy.bat
+++ deploy.bat
@@ -4,5 +4,6 @@ git add .
 set /p commit_msg="Enter commit message: "
 git commit -m "%commit_msg%"
 echo [2/3] Pushing to GitHub...
-git push
+:: 修改這行，明確指定推送到 main
+git push origin master:main
 pause
