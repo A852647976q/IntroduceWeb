a3ba9141bbd3b8f1d00774578e24ee5d31291cf0
A852647976q <serens9528@gmail.com>
2026-03-31 01:23:16 +0800

    1234

--- deploy.bat
+++ deploy.bat
@@ -3,10 +3,6 @@ echo [1/3] Adding changes...
 git add .
 set /p commit_msg="Enter commit message: "
 git commit -m "%commit_msg%"
-
-echo [2/3] Pushing to GitHub (master to main)...
-:: 指定將本地 master 推送到遠端 origin 的 main 分支
-git push origin master:main
-
-echo [3/3] Done!
+echo [2/3] Pushing to GitHub...
+git push
 pause
