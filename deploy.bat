3698040c53fd4c91cb8f8fdc924a8f610f5ff12b
A852647976q <serens9528@gmail.com>
2026-03-31 19:04:27 +0800

    11111

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,10 @@ echo [1/3] Adding changes...
 git add .
 set /p commit_msg="Enter commit message: "
 git commit -m "%commit_msg%"
+
 echo [2/3] Pushing to GitHub...
-git push
+:: 指定推送到 origin 的 main 分支
+git push origin main
+
+echo [3/3] Done! Remember to run ./update.sh on GCP.
 pause
