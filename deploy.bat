17b13f0f8d9732bcb6da84001561cd7f70d1017d
A852647976q <serens9528@gmail.com>
2026-04-21 00:24:21 +0800

    Auto deploy: 2026/04/21 週二  0:24:21.62

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,20 @@
 @echo off
-echo [1/3] Adding changes...
-git add .
-set /p commit_msg="Enter commit message: "
-git commit -m "%commit_msg%"
-echo [2/3] Pushing to GitHub...
-git push
-pause
+echo 開始執行自動化部署推送...
 
+:: 1. 進入正確的資料夾 (預防路徑跑掉)
+cd /d %~dp0
+
+:: 2. 強制把所有變更加入 (包含新檔案)
+git add -A
+
+:: 3. 提交變更 (加上時間戳記，避免訊息重複)
+set msg="Auto deploy: %date% %time%"
+git commit -m %msg%
+
+:: 4. 推送到 GitHub
+git push origin main
+
+echo.
+echo ---------------------------------------
+echo 任務完成！請去 GitHub Actions 看看有沒有綠勾勾。
+pause
